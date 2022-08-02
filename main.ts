7db32aa84463d5d3c73037f185dc252c160fa91a
leonardo-daryanto <103314397+leonardo-daryanto@users.noreply.github.com>
2022-08-02 14:22:11 +0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
-})
+namespace serial {
+    /**
+     *  make a console type
+     */
+    //% block
+    //% group="Advanced"
+    export function showConsoleString(stringType: string): void {
+        
+        serial.writeLine(stringType)
+    }
+}
